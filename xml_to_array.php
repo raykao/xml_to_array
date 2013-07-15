4730ab02bde9615ac8284b58b9c9ce044924074f
Raymond Kao <ray@peopleandcode.com>
2013-07-15 11:15:11 -0400

    inital commit

---
+++ xml_to_array.php
@@ -0,0 +1,9 @@
+<?php
+
+function xml2array ($xmlObject, $out = array ()){
+	foreach ( (array) $xmlObject as $index => $node )
+			$out[$index] = ( is_object ( $node ) ) ? xml2array ( $node ) : $node;
+	return $out;
+}
+
+?>
